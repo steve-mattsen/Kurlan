507d3049a93f13da7c2c660dc73c40d0871e5b32
Steve Mattsen <email@steef.us>
2020-03-02 14:38:12 -0600

    Moved the default state to another file. Fixed references to personas.

--- App.tsx
+++ App.tsx
@@ -2,76 +2,13 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View,ScrollView, Picker, Image, FlatList, StatusBar,ImageBackground,TouchableHighlight, Platform} from 'react-native';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 
-var personas = [
-	{
-		id: 0,
-		name: 'Assistant',
-		icon: require('./assets/personas/assistant.png'),
-	},{
-		id: 1,
-		name: 'Guy',
-		icon: require('./assets/personas/guy.png'),
-	},{
-		id: 2,
-		name: 'Sad Donkey',
-		icon: require('./assets/personas/sadDonkey.png'),
-	},{
-		id: 3,
-		name: 'Chicken with no head',
-		icon: require('./assets/personas/chicken.png'),
-	},{
-		id: 4,
-		name: 'Inner Child',
-		icon: require('./assets/personas/innerChild.png'),
-	},{
-		id: 5,
-		name: 'Inner Parent',
-		icon: require('./assets/personas/father.png'),
-	}
-];
-
-var messages = [
-	{
-		key: 0,
-		persona: 0,
-		text: "Good morning. How are you doing today?",
-	},{
-		key: 1,
-		persona: 1,
-		text: "I'm depressed and stressed.",
-	},{
-		key: 2,
-		persona: 0,
-		text: "Why's that?",
-	},{
-		key: 3,
-		persona: 3,
-		text: "I have to do like 10 major things today.",
-	},{
-		key: 4,
-		persona: 2,
-		text: "There's so much to do that I just break down and don't do anything.",
-	},{
-		key: 5,
-		persona: 4,
-		text: "I just want to have fun all day and not do work.",
-	},{
-		key: 6,
-		persona: 5,
-		text: "Maybe I can just take out the trash and see how I feel.",
-	}
-];
 
 var app = null;
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		app = this;
-		this.state = Object.assign({}, {
-			channel: '#general',
-			personas: personas,
-			messages: messages,
-		});
+		this.state = require('./DefaultState.tsx').state;
 	}
 	addMessage(persona, message) {
 		this.setState((prev) => {
@@ -91,7 +28,7 @@ export default class App extends Component {
 				<SideBar />
 				<TopBar channel={this.state.channel}/>
 				<MessageContainer messages={this.state.messages} personas={this.state.personas}/>
-				<BottomBar personas={this.state.personas}/>
+				<BottomBar messages={this.state.messages} personas={this.state.personas}/>
 				<KeyboardSpacer />
 			</View>
 		);
@@ -126,7 +63,7 @@ class MessageContainer extends Component {
 		return (
 			<FlatList style={styles.messageContainer} 
 				data={this.props.messages}
-				renderItem={({item}) => <Message persona={item.persona}>{item.text}</Message>}
+				renderItem={({item}) => <Message persona={this.props.personas[item.persona]}>{item.text}</Message>}
 				keyExtractor={(item) => item.key.toString()}
 				/>
 		);
@@ -135,7 +72,7 @@ class MessageContainer extends Component {
 
 class Message extends Component {
 	render() {
-		let p = personas[this.props.persona];
+		let p = this.props.persona;
 		var styleMix = [];
 		if (p.id == 0) {
 			styleMix.push(styles.messageAssistant);
